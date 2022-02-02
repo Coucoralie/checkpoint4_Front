71a9830afe39b7f6cbea43a7ab0d9eeffa92e423
Coucoralie <c.cocourant@gmail.com>
2022-02-02 16:03:47 +0100

    creation of Hero component

---
+++ checkpoint_front/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+/** Import des images */
+import Hero from '../assets/images/hero.jpeg';
+
+const Hero = () => {
+  return(
+    <div>
+      <img src ={Hero} alt="hero" />
+    </div>
+  )
+}
