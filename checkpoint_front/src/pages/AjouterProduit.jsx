ee99208df3a709ddf9b7ce667f44c9789c4df178
Coucoralie <c.cocourant@gmail.com>
2022-02-03 17:08:31 +0100

    created Form component

--- checkpoint_front/src/pages/AjouterProduit.jsx
+++ checkpoint_front/src/pages/AjouterProduit.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+/** Import des composants */
+import Form from "../components/Form";
 
 const AjouterProduit = () => {
   return(
     <div>
       <h1>AjouterProduit</h1>
+      <Form />
     </div>
   )
 };
