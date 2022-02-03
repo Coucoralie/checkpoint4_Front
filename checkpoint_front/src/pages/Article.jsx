383534b248dbb1081221e0ca201b85df52ffd690
Coucoralie <c.cocourant@gmail.com>
2022-02-03 13:58:38 +0100

    fetched produits/:id data in Article page

--- checkpoint_front/src/pages/Article.jsx
+++ checkpoint_front/src/pages/Article.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+
 const Article = () => {
   const [myProduit, setMyProduit] = useState({});
-  const { id } = useParams;
-  const navigate = useNavigate();
+  const { id } = useParams();
+  // const navigate = useNavigate();
+  /**
+   * Fetch des données
+ */
+useEffect(() => {
+  axios
+    .get( `http://localhost:5000/produits/${id}`)
+    .then(response => 
+      response.data[0])
+    .then((data) => {
+      setMyProduit(data);
+  })
+    .catch((err) => console.error(err));
+}, []);
+  console.log(myProduit);
   return (
     <div>
       <h1>Article</h1>
-      <p>{myProduit.name}</p>
     </div>
   );
 };
