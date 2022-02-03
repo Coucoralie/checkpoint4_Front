3e37fc34e59ef5a145fedf3572a150740cd0af50
Coucoralie <c.cocourant@gmail.com>
2022-02-03 11:19:41 +0100

    added CarteProduits component in Boutique page

--- checkpoint_front/src/pages/Boutique.jsx
+++ checkpoint_front/src/pages/Boutique.jsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
 import fetchAllCategories from "../services/categories";
-
+import fetchAllProduits from "../services/produits";
+/** Import de CSS */
+import "./Boutique.css";
+/** Import des composants */
+import CarteProduits from "../components/CarteProduits";
 const Boutique = () => {
   const [categories, setCategories] = useState([]);
+  const [produits, setProduits] = useState([]);
+  const [categorieFiltered, setCategorieFiltered] = useState("");
 
    /**
    * Fetch des données
    */
-    const getData = () => {
+    const getCategories = () => {
       fetchAllCategories()
         .then((data) => {
           setCategories(data);
@@ -16,14 +22,46 @@ const Boutique = () => {
         .catch((err) => console.error(err));
     };
     useEffect(() => {
-      getData();
+      getCategories();
+    }, []);
+    console.log(categories);
+    const getProduits = () => {
+      fetchAllProduits()
+        .then((data) => {
+          setProduits(data);
+          console.log(data);
+        })
+        .catch((err) => console.error(err));
+    };
+    useEffect(() => {
+      getProduits();
     }, []);
     console.log(categories);
   return(
     <div className="boutique">
-      <h1>Boutique</h1>
+      <h3 className="title-boutique">Boutique</h3>
+      <div className="input-boutique">
+      <label htmlFor="search" className="search">
+        Rechercher par catégorie
+        <input type ="text" value={categorieFiltered} onChange={(e) => setCategorieFiltered(e.target.value)} className="input"/>
+      </label>
+      </div>
+      <div className="card-container">
+      {/* {categories.filter((value) => {
+        if(categorieFiltered === ''){
+          return value;
+        }else if( value.name.toLowerCase().includes(categorieFiltered.toLowerCase())){
+          return value;
+        }
+        console.log(value);
+      })} */}
+      {produits.map((produit) => (
+        <CarteProduits produit={produit} />
+      ))}
+      </div>
     </div>
   )
 };
 
 export default Boutique;
+ 
