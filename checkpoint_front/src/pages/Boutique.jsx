ed80de051d5a75b2f847553ceae32310818384bd
Coucoralie <c.cocourant@gmail.com>
2022-02-03 09:37:07 +0100

    data consumed in Boutique page

--- checkpoint_front/src/pages/Boutique.jsx
+++ checkpoint_front/src/pages/Boutique.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import fetchAllCategories from "../services/categories";
 
 const Boutique = () => {
+  const [categories, setCategories] = useState([]);
+
+   /**
+   * Fetch des données
+   */
+    const getData = () => {
+      fetchAllCategories()
+        .then((data) => {
+          setCategories(data);
+          console.log(data);
+        })
+        .catch((err) => console.error(err));
+    };
+    useEffect(() => {
+      getData();
+    }, []);
+    console.log(categories);
   return(
     <div className="boutique">
       <h1>Boutique</h1>
